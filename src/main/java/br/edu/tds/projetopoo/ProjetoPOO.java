4290f44c17749e2f7f3fac2262fbef610935b4c5
Juan <juan.vieira@alunos.ifsuldeminas.edu.br>
2026-02-09 16:59:46 -0300

    Primeiro código do ProjetoPOO

---
+++ src/main/java/br/edu/tds/projetopoo/ProjetoPOO.java
@@ -0,0 +1,13 @@
+package br.edu.tds.projetopoo;
+
+/**
+ *
+ * @author aluno
+ */
+public class ProjetoPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
