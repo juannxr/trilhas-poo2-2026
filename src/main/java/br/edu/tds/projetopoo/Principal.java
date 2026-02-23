2dfaa2628ad2314172f8fdd7195517489bf35e0c
Juan <juan.vieira@alunos.ifsuldeminas.edu.br>
2026-02-23 14:14:25 -0300

    Classe Cliente adicionada

---
+++ src/main/java/br/edu/tds/projetopoo/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.tds.projetopoo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Principal {
+    public static void main(String[] args){
+        System.out.println("Testando objetos da classe cliente");
+        //Usando a classe cliente 
+     
+        Cliente c1 = new Cliente("Davi", 19, "123456789");
+        c1.imprimeDadosCliente();
+    }
+}
